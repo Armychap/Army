0c8653601c6206556e16e39bd88b29cb9517af9d
Mariia <marry.zv@gmail.com>
2026-02-27 17:04:51 +0300

    .

--- 3 задание/obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ 3 задание/obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ae495f7c49f2cca325d332b29aaedd73de0215")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf525e7ae31fd3f0d11158064515de3f0bf5afc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
